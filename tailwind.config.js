341371ff663f10204122cc4eef28c16cf9037248
Diallo Hudgins <d@diallohudgins.com>
2025-01-15 15:18:12 -0500

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./templates/**/*.{html,twig}"  // adjust based on your template locations
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
